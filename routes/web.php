0209bc9ea6a8308f077e7f2c6fd1db61c40f4036
Ashley3006 <ashashey86@gmail.com>
2025-03-21 18:25:55 +0100

    creation des controllers et routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\Admin\oeuvreControllerController as AdminoeuvreController;
+use App\Http\Controllers\Admin\categorieControllerController as AdmincategorieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/admin/oeuvre/ajouter', [AdminOeuvreController::class, 'ajouter'])->name('admin.oeuvre.ajouter');
+
+Route::post('/admin/oeuvre/ajouter/traitement', [AdminoeuvreController::class, 'ajouter_traitement'])->name('admin.oeuvres.ajouter.traitement');
+
+Route::get('/admin/oeuvre/{id}/modifier', [AdminoeuvreController::class, 'modifier'])->name('admin.oeuvre.modifier');
+
+Route::post('/admin/oeuvre/modifier/{id}/traitement', [AdminoeuvreController::class, 'modifier_traitement'])->name('admin.oeuvres.modifier_traitement');
+
+Route::get('/admin/oeuvre/liste', [AdminoeuvreController::class, 'liste'])->name('admin.oeuvres.liste');
+
+Route::post('/admin/oeuvre/{id}/supprimer', [AdminoeuvreController::class, 'supprimer'])->name('admin.oeuvre.supprimer');
+
+Route::get('/admin/oeuvre/detail', [AdminoeuvreController::class, 'detail'])->name('admin.oeuvre.detail');
+
+
